abdf3ca1936d7599c71141cf6f400d8d07d9a883
Bruno-ferrariv <brunofvincensi@gmail.com>
2021-08-03 21:36:39 -0300

    atualizacao

--- java-oo/src/br/com/javaoo/Principal.java
+++ java-oo/src/br/com/javaoo/Principal.java
@@ -1,36 +1,47 @@
 package br.com.javaoo;
 
+
 import br.com.javaoo.entities.Product;
+
+import java.util.Scanner;
 
 public class Principal {
     public static void main(String[] args) {
 
 
-        Product eletrodomestico = new Product();
-
-        eletrodomestico.setName("Geladeira");
-        eletrodomestico.setPrice(1.200);
-        eletrodomestico.setQuantity(2);
-
-        System.out.println(eletrodomestico.getName());
-        System.out.println(eletrodomestico.getPrice());
-        System.out.println(eletrodomestico.getQuantity());
+        Scanner sc = new Scanner(System.in);
 
 
-        Product mesa = new Product();
+        Product p = new Product();
 
-        mesa.setName("Mesa de Jantar");
-        mesa.setPrice(800);
-        mesa.setQuantity(10);
+        System.out.println("Enter product data: ");
 
-        System.out.println(mesa.getName());
-        System.out.println(mesa.getPrice());
-        System.out.println(mesa.getQuantity());
+        System.out.println("Name: ");
+        p.setName(sc.nextLine());
+        System.out.println("Price: ");
+        p.setPrice(sc.nextDouble());
+        System.out.println("Quantity: ");
+        p.setQuantity(sc.nextInt());
+
+        System.out.println("Product data: " + p.getName() + " $ " + p.getPrice() + " " + p.getQuantity() + " units, "
+                +  "Total: $" + p.totalValueInStock());
 
 
+        System.out.println("Enter the number of products to be added in stock: ");
+        p.addProduct(sc.nextInt());
 
 
 
+        System.out.println("Updated data: " + p.getName() + " $ " + p.getPrice()  + p.getQuantity() + " units, "
+                +  "Total: $" + p.totalValueInStock());
 
+        System.out.println("Enter the number of products to be removed from stock: ");
+        p.removeProduct(sc.nextInt());
+
+        System.out.println("Updated data: " + p.getName() + " $ " + p.getPrice()  + p.getQuantity() + " units, "
+                +  "Total: $" + p.totalValueInStock());
+
+
+sc.close();
     }
 }
--- java-oo/src/br/com/javaoo/entities/Product.java
+++ java-oo/src/br/com/javaoo/entities/Product.java
@@ -6,46 +6,73 @@ public class Product {
     private String name;
     private double price;
     private int quantity;
-
+    private String color;
 
     //metodos acessores
+    //GETTERS and SETTERS
+
     public void setName(String nome){
-
         this.name = nome;
-
     }
 
     public String getName(){
-
         return name;
     }
 
 
-
-    public void setPrice(double price){
-
-        this.price = price;
-
+    public void setPrice(double preco){
+        this.price = preco;
     }
-
     public double getPrice(){
-
         return price;
     }
-
 
 
     public void setQuantity(int quantidade){
 
         this.quantity = quantidade;
-
     }
-
     public int getQuantity(){
-
         return quantity;
     }
+
+    public void setColor(String cor){
+        this.color = cor;
+    }
+    public String getColor(){
+        return color;
+    }
+
+
+    public double totalValueInStock(){
+
+        return price * quantity;
+    }
+
+
+
+    public void addProduct(int quantity){
+
+        if(quantity < 0){
+
+            return;
+        }
+        this.quantity += quantity;
+
+    }
+
+    public void removeProduct(int quantity){
+
+        if(quantity > this.quantity){
+            return;
+
+        }
+        this.quantity -= quantity;
+
+    }
+
 
 
 
 }
+
