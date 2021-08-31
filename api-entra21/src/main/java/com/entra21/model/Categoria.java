95cfdbd200e2621441cecd8118999a710d76637f
Bruno-ferrariv <brunofvincensi@gmail.com>
2021-08-31 16:06:06 -0300

    java com framework

---
+++ api-entra21/src/main/java/com/entra21/model/Categoria.java
@@ -0,0 +1,46 @@
+package com.entra21.model;
+
+import java.util.Objects;
+
+public class Categoria {
+
+    private Long id;
+    private String descricao;
+
+    public Categoria() {
+    }
+
+    public Categoria(Long id, String descricao) {
+        this.id = id;
+        this.descricao = descricao;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Categoria categoria = (Categoria) o;
+        return id.equals(categoria.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public String toString() {
+        return "Categoria{" +
+                "id=" + id +
+                ", descricao='" + descricao + '\'' +
+                '}';
+    }
+}
---
+++ api-entra21/src/test/java/com/entra21/ApiEntra21ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.entra21;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiEntra21ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
